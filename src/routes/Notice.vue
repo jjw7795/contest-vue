8e9d4be14e15b287af72700b9bb3a31a7e616296
jjw7795 <jjw7795@naver.com>
2021-10-23 01:40:40 +0900

    axios 테스트

--- src/routes/Notice.vue
+++ src/routes/Notice.vue
@@ -2,7 +2,7 @@
   <div class="notice">
     <TitleSearch title="공지사항" />
     <div class="count-box">
-      <p>총 1건</p>
+      <p>총 1건 {{ h }}</p>
     </div>
 
     <Table primary="번호" />
@@ -14,6 +14,7 @@
 import TitleSearch from "~/components/TitleSearch";
 import Table from "~/components/Table";
 import Pagination from "~/components/Pagination";
+import axios from "axios";
 
 export default {
   components: {
@@ -24,7 +25,23 @@ export default {
   data() {
     return {
       pages: [1, 2, 3, 4, 5],
+      h: "",
+      member: {
+        id: "1",
+        name: "jinwoo",
+      },
     };
+  },
+  mounted() {
+    axios.get("http://localhost:8090/aaa").then((res) => {
+      console.log(res.data);
+      this.h = res.data;
+    });
+    axios
+      .post("http://localhost:8090/post", JSON.stringify(this.member))
+      .then((res) => {
+        console.log(res.data);
+      });
   },
 };
 </script>
